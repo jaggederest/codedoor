79d5013d745eca8c31046cde3c8d9b557b306031
Ronald Cheng <rhc2104@columbia.edu>
2013-08-12 20:19:00 +0800

    Add ability to log out

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 feature 'Logging in' do
-  scenario 'Successful log in' do
+  scenario 'Successful log in and log out' do
     visit '/'
     expect(page).to have_content 'Log in with GitHub'
     expect(page).to have_content 'Work for clients that appreciate top developers that contribute to open source. Daily billing.'
     expect(page).to have_content 'Find programmers that have contributed to meaningful open source projects, and see their code samples.'
     click_link 'Log in with GitHub'
-    expect(page).to have_content 'Logged in'
+    expect(page).to have_content 'Log out'
+
+    click_link 'Log out'
+    expect(page).to have_content 'Log in with GitHub'
   end
 
   scenario 'Failed log in' do

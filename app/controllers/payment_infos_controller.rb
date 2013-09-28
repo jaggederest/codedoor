d0edfe2e3b00163a29c46f69a18c651726683fb3
Ronald Cheng <rhc2104@columbia.edu>
2013-09-27 17:49:18 -0700

    Be more graceful in error case (as it is an array, not a hash)

--- app/controllers/payment_infos_controller.rb
+++ app/controllers/payment_infos_controller.rb
@@ -10,22 +10,22 @@ class PaymentInfosController < ApplicationController
   def create
     if params[:error]
       error_array = []
-      params[:error].each_value { |value| error_array << value.to_s unless value.blank? }
+      params[:error].each { |value| error_array << value.to_s unless value.blank? }
 
       flash[:error] = error_array
 
-      render json: {redirect_to: new_user_payment_info_url(params["user_id"].to_i)}
+      render json: {redirect_to: new_user_payment_info_url(params[:user_id].to_i)}
     elsif params[:data][:security_code_check] == "failed"
       flash[:error] = ["Your security code is invalid, please try again."]
 
-      render json: {redirect_to: new_user_payment_info_url(params["user_id"].to_i)}
+      render json: {redirect_to: new_user_payment_info_url(params[:user_id].to_i)}
     else
       card_uri = params[:data][:uri]
       PaymentInfo.find_or_create_by(user_id: current_user.id).associate_card(card_uri)
 
       flash[:notice] = "Your payment details have been successfully saved."
 
-      render json: {redirect_to: new_user_payment_info_url(params["user_id"].to_i)}
+      render json: {redirect_to: new_user_payment_info_url(params[:user_id].to_i)}
     end
   end
 end

82d6bf67f3c909c41f9d8d3db321102c21eaa232
Ronald Cheng <rhc2104@columbia.edu>
2013-11-03 08:32:53 -0800

    Add integration test for redirect flow

--- spec/features/job_spec.rb
+++ spec/features/job_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 feature 'Job setup', js: true do
+  before :each do
+    @programmer = FactoryGirl.create(:programmer, :qualified, visibility: 'public', user: FactoryGirl.create(:user_checked_terms, full_name: 'Test Programmer'))
+  end
+
   scenario 'client searches for programmer and offers job' do
     client_sign_up
-    user = FactoryGirl.create(:user_checked_terms, full_name: 'Test Programmer')
-    FactoryGirl.create(:programmer, :qualified, visibility: 'public', user: user)
     click_link 'Search'
 
     click_link 'Test Programmer'
@@ -30,5 +32,30 @@ feature 'Job setup', js: true do
     click_link 'Finish Job'
 
     page.should have_content 'Job Finished'
+  end
+
+  scenario 'client searched for programmer while logged out' do
+    visit '/'
+    click_button 'Search'
+
+    click_link 'Test Programmer'
+
+    click_link 'Contact'
+
+    # Contact button requires login, so put into oAuth flow, and then create user page
+    page.should have_content 'By checking this box, I agree to abide by CodeDoor\'s'
+    check('user_checked_terms')
+    select('United States', from: 'user_country')
+    select('California', from: 'user_state')
+    fill_in 'City', with: 'Burlingame'
+    click_button('Create Client Account')
+
+    fill_in 'Company', with: 'Test Company'
+    fill_in 'Description', with: 'Test Description'
+    click_button 'Add Info'
+
+    # After creating a client, you should be redirect back to the new job flow
+    page.should have_content 'Job Name'
+    page.should have_content 'Message'
   end
 end
